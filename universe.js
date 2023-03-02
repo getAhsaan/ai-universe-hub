a7ec996a5a6772310b7e57f1b17e4d1a16807c25
getahsaan <getahsaan@gmail.com>
2023-03-02 22:35:52 +0600

    dynamically card and card details added

--- universe.js
+++ universe.js
@@ -1,2 +1,43 @@
 
+const getToolsData = () => {
+    fetch(' https://openapi.programming-hero.com/api/ai/tools')
+        .then(res => res.json())
+        .then(data => showToolsData(data.data.tools))
+};
+getToolsData();
 
+const showToolsData = (data) => {
+    // console.log(data);
+    data.forEach(toolData => {
+        // console.log(toolData);
+        const { image, features, name, published_in: date } = toolData;
+        const cardContainer = document.getElementById('card-container');
+        cardContainer.innerHTML += `
+         <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="card">
+                            <img src="${image}" class="card-img-top img-fluid"
+                                style="width: 437px; height: 300px;" alt="...">
+                            <div class="card-body">
+                                <h4>Features</h4>
+                                <ol class="list list-numbered">
+                                ${features.map(f => `<li class="list-item">${f}</li>`).join(' ')}
+                                </ol>
+                                <hr>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <h4>${name}</h4>
+                                        <i class="fa-regular fa-calendar"></i> <span>${date}</span>
+                                    </div>
+                                    <div>
+                                        <i class="fa-solid fa-arrow-right bg-warning-subtle btn text-warning rounded-circle p-2"></i>
+                                
+                                    </div>
+                                </div>
+                            </div>
+
+                           
+                        </div>
+                    </div>
+    `
+    });
+}
